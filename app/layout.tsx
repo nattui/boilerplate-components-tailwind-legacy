2432066efd8ab4a47abbecbefd18bb165d77e3b2
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-26 13:13:13 -0800

    type

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
+import type { PropsWithChildren } from "react"
 import { Geist, Geist_Mono } from "next/font/google"
-import { PropsWithChildren } from "react"
 import "./globals.css"
 
 const geistSans = Geist({
