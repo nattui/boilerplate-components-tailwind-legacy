341540d31c329814300002c33fad8e15656a2420
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-26 23:05:00 -0800

    input page

--- app/inputs/page.tsx
+++ app/inputs/page.tsx
@@ -1,11 +1,12 @@
+import Button from "@/components/button"
 import { Input } from "@/components/input"
 
 export default function InputsPage() {
   return (
     <div className="flex h-[100dvh] flex-col items-center gap-16 overflow-y-auto py-128">
-      {/* Input */}
-      <div className="flex w-320 items-center justify-center gap-8">
+      <div className="flex w-320 flex-col items-center justify-center gap-8">
         <Input defaultValue="Hello world" fullWidth />
+        <Button fullWidth size="large" text="Continue" />
       </div>
     </div>
   )
--- app/topbar.tsx
+++ app/topbar.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "@/components/nav-link"
 
 export default function Topbar() {
   return (
-    <div className="flex items-center gap-8 p-64">
+    <div className="flex items-center p-64">
       <NavLink href="/">Home</NavLink>
       <NavLink href="/buttons">Buttons</NavLink>
       <NavLink href="/inputs">Inputs</NavLink>
