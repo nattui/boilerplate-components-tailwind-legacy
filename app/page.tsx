0a59c76ba8355e82572cfa71abb0455a11d9a2f9
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-26 16:39:08 -0800

    buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Button from "@/components/button"
 
 export default function Home() {
   return (
-    <div className="flex h-[100lvh] items-center justify-center">
-      <Button>Button</Button>
+    <div className="d flex h-full items-center justify-center">
+      <div className="flex items-center justify-center gap-4">
+        <Button size="small">Small</Button>
+        <Button size="medium">Medium</Button>
+        <Button size="large">Large</Button>
+      </div>
     </div>
   )
 }
--- components/button.tsx
+++ components/button.tsx
@@ -4,9 +4,9 @@ export const buttonStyles = {
   base: [
     "active:bg-mauve-4",
     "active:text-mauve-12",
-    "border",
     "border-mauve-a4",
     "border-solid",
+    "border",
     "cursor-pointer",
     "data-[active=true]:bg-mauve-4",
     "data-[active=true]:text-mauve-12",
@@ -16,23 +16,35 @@ export const buttonStyles = {
     "hover:text-mauve-12",
     "items-center",
     "justify-center",
-    "px-12",
-    "py-6",
     "rounded-6",
-    "select-none",
     "shrink-0",
-    "text-15",
     "transition-colors",
   ].join(" "),
+  size: {
+    large: ["px-12", "text-16", "h-36"].join(" "),
+    medium: ["px-8", "text-15", "h-32"].join(" "),
+    small: ["px-6", "text-14", "h-28"].join(" "),
+  },
 }
 
-type ButtonProps = ButtonHTMLAttributes<ButtonRef>
+type ButtonProps = ButtonHTMLAttributes<ButtonRef> & {
+  size?: "large" | "medium" | "small"
+}
 type ButtonRef = HTMLButtonElement
 
 const Button = forwardRef<ButtonRef, ButtonProps>((props, ref) => {
-  const { className: customStyles = "", type = "button", ...rest } = props
+  const {
+    className: customStyles = "",
+    size = "medium",
+    type = "button",
+    ...rest
+  } = props
 
-  const combinedStyles = `${buttonStyles.base} ${customStyles}`.trim()
+  const combinedStyles = `
+    ${buttonStyles.base}
+    ${buttonStyles.size[size]}
+    ${customStyles}
+  `.trim()
 
   return <button className={combinedStyles} ref={ref} type={type} {...rest} />
 })
