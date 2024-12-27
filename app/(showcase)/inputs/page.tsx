7b1685bbac8d0e29ce5d51f255ea073fd23c16c4
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-26 23:18:02 -0800

    form

--- app/(showcase)/inputs/page.tsx
+++ app/(showcase)/inputs/page.tsx
@@ -1,15 +1,32 @@
+"use client"
+
 import Button from "@/components/button"
 import { Input } from "@/components/input"
+import { FormEvent } from "react"
 
 export default function InputsPage() {
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
   return (
     <div className="flex h-[100dvh] flex-col items-center gap-16 overflow-y-auto py-128">
-      <div className="flex w-320 flex-col items-center justify-center gap-8">
-        <Input defaultValue="Hello world" fullWidth />
-        <Button fullWidth size="large">
+      <form className="flex w-320 flex-col" onSubmit={onSubmit}>
+        <label className="mb-8 w-fit text-13" htmlFor="name">
+          Your Name
+        </label>
+        <Input
+          autoFocus
+          className="mb-16"
+          fullWidth
+          id="name"
+          name="name"
+          type="text"
+        />
+        <Button fullWidth size="large" type="submit">
           Continue
         </Button>
-      </div>
+      </form>
     </div>
   )
 }
