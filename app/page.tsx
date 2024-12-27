bb37a4f92f46088f2ee23785437bd219e95d0550
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-26 16:52:43 -0800

    icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
 import Button from "@/components/button"
+import { AnchorSimple } from "@phosphor-icons/react/dist/ssr"
 
 export default function Home() {
   return (
     <div className="flex h-[100dvh] items-center justify-center">
       <div className="flex items-center justify-center gap-4">
-        <Button size="small">Small</Button>
-        <Button size="medium">Medium</Button>
-        <Button size="large">Large</Button>
+        <Button size="small">
+          <div className="flex items-center gap-4">
+            <AnchorSimple size={14} />
+            <span>Small</span>
+          </div>
+        </Button>
+        <Button size="medium">
+          <div className="flex items-center gap-6">
+            <AnchorSimple size={16} />
+            <span>Medium</span>
+          </div>
+        </Button>
+        <Button size="large">
+          <div className="flex items-center gap-8">
+            <AnchorSimple size={18} />
+            <span>Large</span>
+          </div>
+        </Button>
       </div>
     </div>
   )
