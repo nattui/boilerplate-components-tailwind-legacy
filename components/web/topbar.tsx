137e0e09dc1a8bf89e31d6474ede6d91e6d5977a
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-27 02:39:19 -0800

    flex wrap

--- components/web/topbar.tsx
+++ components/web/topbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "@/components/web/nav-link"
 
 export default function Topbar() {
   return (
-    <div className="flex items-center px-64 py-32">
+    <div className="flex flex-wrap items-center px-64 py-32">
       <NavLink href="/">Home</NavLink>
       <NavLink href="/buttons">Buttons</NavLink>
       <NavLink href="/inputs">Inputs</NavLink>
