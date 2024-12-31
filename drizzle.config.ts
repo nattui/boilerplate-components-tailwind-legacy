ad8c6a811d871e7761d66ad889cf79e2a068d525
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-31 01:58:41 -0800

    minor

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "drizzle-kit"
-
 // ====================================================== /
 // :: Notes ::
 // pnpm run db:generate
 // pnpm run db:migrate
 // ====================================================== /
+
+import { defineConfig } from "drizzle-kit"
 
 if (!process.env.DATABASE_URL) {
   throw new Error("`DATABASE_URL` environment variable is required.")
