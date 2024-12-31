df12aec13b81d12d20f6bfdea26d7184d27f979a
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-31 02:27:18 -0800

    minor

--- app/(showcase)/users/page.tsx
+++ app/(showcase)/users/page.tsx
@@ -5,7 +5,7 @@ export default async function UsersPage() {
   const users = await db.select().from(usersTable)
 
   return (
-    <div className="flex flex-col gap-32 p-32">
+    <div className="flex flex-col gap-16 p-32">
       {users.map((user) => (
         <div
           className="shadow-sm flex flex-col gap-4 rounded-8 border border-mauve-a6 p-16 font-mono text-14 shadow-1"
