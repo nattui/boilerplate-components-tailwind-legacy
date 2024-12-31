cccf64d134e6ff75493cf6b61023a9b410c1908d
Natt Nguyen <natt.lefevre@gmail.com>
2024-12-31 02:08:10 -0800

    styled

--- app/(showcase)/users/page.tsx
+++ app/(showcase)/users/page.tsx
@@ -5,8 +5,18 @@ export default async function UsersPage() {
   const users = await db.select().from(usersSchema)
 
   return (
-    <div>
-      <pre>{JSON.stringify(users, undefined, 2)}</pre>
+    <div className="flex flex-col gap-32 p-32">
+      {users.map((user) => (
+        <div
+          className="shadow-sm flex flex-col gap-4 rounded-8 border border-mauve-a6 p-16 font-mono text-14 shadow-1"
+          key={user.id}
+        >
+          <div>
+            {user.id} {user.fullName}
+          </div>
+          <div>{user.phone}</div>
+        </div>
+      ))}
     </div>
   )
 }
