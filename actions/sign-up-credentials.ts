28728110e64cd60b5debbeb2ee29e578b9c2dd92
Natt Nguyen <natt.lefevre@gmail.com>
2025-01-01 01:03:50 -0800

    sign up action

---
+++ actions/sign-up-credentials.ts
@@ -0,0 +1,111 @@
+"use server"
+
+import { db } from "@/libs/db"
+import {
+  authCredentialsTable,
+  authProvidersTable,
+  usersTable,
+} from "@/libs/db/schema"
+import { encrypt } from "@/libs/session"
+import { signInSchema } from "@/libs/validation/sign.schema"
+import { EXPIRATION_TIME_IN_SECONDS, MESSAGE, ROUTE } from "@/utils/constants"
+import { isDevelopment } from "@/utils/is-development"
+import { hash } from "bcrypt"
+import { and, eq } from "drizzle-orm"
+import { cookies as nextCookies } from "next/headers"
+import { redirect } from "next/navigation"
+
+export async function signUpCredentials(_: unknown, formData: FormData) {
+  const email = formData.get("email") as string
+  const password = formData.get("password") as string
+
+  // Validate form data
+  const result = signInSchema.safeParse({ email, password })
+  if (!result.success) {
+    const error = result.error.flatten().fieldErrors
+    return { message: error.email?.[0] || error.password?.[0] }
+  }
+
+  // Get user from database
+  const [existingUser] = await db
+    .select({
+      email: usersTable.email,
+      hashPassword: authCredentialsTable.hashPassword,
+      id: usersTable.id,
+    })
+    .from(usersTable)
+    .innerJoin(authProvidersTable, eq(authProvidersTable.userId, usersTable.id))
+    .innerJoin(
+      authCredentialsTable,
+      eq(authCredentialsTable.providerId, authProvidersTable.id),
+    )
+    .where(
+      and(
+        eq(usersTable.email, email),
+        eq(authProvidersTable.provider, "credentials"),
+      ),
+    )
+
+  if (existingUser) {
+    return { message: MESSAGE.AUTH.SIGNUP.EMAIL_ALREADY_EXISTS }
+  }
+
+  const hashedPassword = await hash(password, 10)
+
+  // TODO: Transaction
+  // Create user
+  const [user] = await db
+    .insert(usersTable)
+    .values({
+      email,
+      name:
+        email.split("@")[0].charAt(0).toUpperCase() +
+        email.split("@")[0].slice(1) +
+        " " +
+        email.split("@")[0].charAt(0).toUpperCase() +
+        email.split("@")[0].slice(1),
+      username: email.split("@")[0],
+    })
+    .returning({
+      email: usersTable.email,
+      id: usersTable.id,
+      name: usersTable.name,
+      username: usersTable.username,
+    })
+
+  const [provider] = await db
+    .insert(authProvidersTable)
+    .values({
+      provider: "credentials",
+      userId: user.id,
+    })
+    .returning({ id: authProvidersTable.id })
+
+  await db.insert(authCredentialsTable).values({
+    hashPassword: hashedPassword,
+    providerId: provider.id,
+  })
+
+  // Create session token
+  const token = await encrypt({
+    email: user.email,
+    id: user.id,
+    name: user.name,
+    username: user.username,
+  })
+
+  // Set session token in cookies
+  const cookies = await nextCookies()
+  cookies.set({
+    httpOnly: true,
+    maxAge: EXPIRATION_TIME_IN_SECONDS,
+    name: "session",
+    path: "/",
+    priority: "medium",
+    sameSite: "lax",
+    secure: !isDevelopment,
+    value: token,
+  })
+
+  redirect(ROUTE.LIFE_EXPECTANCY)
+}
--- app/(auth)/signup/page.client.tsx
+++ app/(auth)/signup/page.client.tsx
@@ -1,64 +1,24 @@
 "use client"
 
+import { signUpCredentials } from "@/actions/sign-up-credentials"
 import Button from "@/components/primitives/button"
 import Input from "@/components/primitives/input"
 import Label from "@/components/primitives/label"
-import { signInSchema } from "@/libs/validation/sign.schema"
-import { API, ROUTE } from "@/utils/constants"
 import { SignIn } from "@phosphor-icons/react"
-import { useRouter } from "next/navigation"
-import { type FormEvent, useState } from "react"
+import { useActionState } from "react"
+const initialState = { message: "" }
 
 export default function SignUpClientPage() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [errorMessage, setErrorMessage] = useState<string>()
-
-  const router = useRouter()
-
-  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    setIsLoading(true)
-
-    const formData = new FormData(event.target as HTMLFormElement)
-    const email = formData.get("email") as string
-    const password = formData.get("password") as string
-
-    const result = signInSchema.safeParse({ email, password })
-    if (!result.success) {
-      const error = result.error.flatten().fieldErrors
-      setErrorMessage(error.email?.[0] || error.password?.[0])
-      setIsLoading(false)
-      return
-    }
-
-    try {
-      const response = await fetch(API.AUTH.SIGNUP_CREDENTIALS, {
-        body: JSON.stringify({ email, password }),
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-      })
-      const data = await response.json()
-
-      if (!response.ok) {
-        setErrorMessage(data.message)
-        setIsLoading(false)
-        return
-      }
-
-      router.push(ROUTE.HOME)
-      router.refresh()
-    } catch (error) {
-      console.log(error)
-      setIsLoading(false)
-    }
-  }
+  const [state, formAction, pending] = useActionState(
+    signUpCredentials,
+    initialState,
+  )
 
   return (
     <div className="flex flex-col">
       <h1 className="mb-16 text-24 font-600">Sign up</h1>
-      {errorMessage && <p className="mb-16 text-red-9">{errorMessage}</p>}
-      <form className="flex w-320 flex-col" onSubmit={handleSubmit}>
+      {state.message && <p className="mb-16 text-red-9">{state.message}</p>}
+      <form action={formAction} className="flex w-320 flex-col">
         <Label className="mb-4" htmlFor="email">
           Email
         </Label>
@@ -87,12 +47,12 @@ export default function SignUpClientPage() {
         />
         <Button
           fullWidth
-          isLoading={isLoading}
+          isLoading={pending}
           leadingVisual={<SignIn size={16} />}
           size="extraLarge"
           type="submit"
         >
-          Sign in
+          Sign up
         </Button>
       </form>
     </div>
