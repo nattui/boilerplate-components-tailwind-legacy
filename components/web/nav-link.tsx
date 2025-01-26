0b24f99ea65f383b3e4bacdec368dd7addac8ff5
Natt Nguyen <natt.lefevre@gmail.com>
2025-01-25 21:05:25 -0800

    fix outline

--- components/web/nav-link.tsx
+++ components/web/nav-link.tsx
@@ -15,7 +15,7 @@ export function NavLink({ children, href }: NavLinkProps) {
 
   return (
     <Link
-      className="rounded-6 text-14 font-500 hover:text-gray-12 focus-visible:outline-primary-9 data-[active=true]:text-gray-12 px-8 py-4 outline-none hover:underline focus-visible:outline-2 focus-visible:outline-offset-2 data-[active=true]:underline"
+      className="rounded-6 text-14 font-500 hover:text-gray-12 focus-visible:outline-primary-9 data-[active=true]:text-gray-12 px-8 py-4 hover:underline focus-visible:outline-2 focus-visible:outline-offset-2 data-[active=true]:underline"
       data-active={isActive}
       href={href}
     >
