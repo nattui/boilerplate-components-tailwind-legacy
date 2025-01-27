624141f95b4dd382a7d6e07b3065721b00f322bc
Natt Nguyen <natt.lefevre@gmail.com>
2025-01-26 19:13:34 -0800

    resize

--- app/(showcase)/textarea/page.tsx
+++ app/(showcase)/textarea/page.tsx
@@ -5,7 +5,7 @@ export default function TextareaPage() {
     <div className="flex w-320 flex-col gap-y-16">
       <Textarea rows={1} />
       <Textarea />
-      <Textarea rows={10} />
+      <Textarea className="resize-none" rows={10} />
     </div>
   )
 }
