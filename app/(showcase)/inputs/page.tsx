e5fb75b12134c8c70f6a9f9610b51518fbb6d431
Natt Nguyen <natt.lefevre@gmail.com>
2025-01-26 19:22:30 -0800

    label

--- app/(showcase)/inputs/page.tsx
+++ app/(showcase)/inputs/page.tsx
@@ -9,52 +9,61 @@ import {
 
 export default function InputsPage() {
   return (
-    <div className="flex w-320 flex-col">
-      <Label className="mb-4" htmlFor="email">
-        Email
-      </Label>
-      <div className="relative mb-16">
-        <Input
-          autoFocus
-          className="pl-36"
-          id="email"
-          name="email"
-          placeholder="email@example.com"
-          required
-          type="email"
-        />
-        <EnvelopeSimple
-          className="text-gray-9 pointer-events-none absolute top-1/2 left-12 -translate-y-1/2"
-          size={16}
-        />
+    <>
+      <div className="flex w-320 flex-col">
+        <Label className="mb-4" htmlFor="email">
+          Email
+        </Label>
+        <div className="relative mb-16">
+          <Input
+            autoFocus
+            className="pl-36"
+            id="email"
+            name="email"
+            placeholder="email@example.com"
+            required
+            type="email"
+          />
+          <EnvelopeSimple
+            className="text-gray-9 pointer-events-none absolute top-1/2 left-12 -translate-y-1/2"
+            size={16}
+          />
+        </div>
+
+        <Label className="mb-4" htmlFor="password">
+          Password
+        </Label>
+        <div className="relative mb-16">
+          <Input
+            className="pl-36"
+            id="password"
+            name="password"
+            placeholder="••••••••"
+            required
+            type="password"
+          />
+          <LockSimple
+            className="text-gray-9 pointer-events-none absolute top-1/2 left-12 -translate-y-1/2"
+            size={16}
+          />
+        </div>
+        <Button
+          fullWidth
+          leadingVisual={<SignIn size={16} />}
+          size="extraLarge"
+          type="submit"
+          variant="accent"
+        >
+          Sign in
+        </Button>
       </div>
 
-      <Label className="mb-4" htmlFor="password">
-        Password
-      </Label>
-      <div className="relative mb-16">
-        <Input
-          className="pl-36"
-          id="password"
-          name="password"
-          placeholder="••••••••"
-          required
-          type="password"
-        />
-        <LockSimple
-          className="text-gray-9 pointer-events-none absolute top-1/2 left-12 -translate-y-1/2"
-          size={16}
-        />
+      <div className="mt-64 flex w-320 flex-col">
+        <Label className="mb-4" htmlFor="disabled">
+          Disabled
+        </Label>
+        <Input disabled id="disabled" name="disabled" placeholder="Disabled" />
       </div>
-      <Button
-        fullWidth
-        leadingVisual={<SignIn size={16} />}
-        size="extraLarge"
-        type="submit"
-        variant="accent"
-      >
-        Sign in
-      </Button>
-    </div>
+    </>
   )
 }
--- app/(showcase)/textarea/page.tsx
+++ app/(showcase)/textarea/page.tsx
@@ -1,11 +1,30 @@
+import Label from "@/components/ui/label"
 import Textarea from "@/components/ui/textarea"
 
 export default function TextareaPage() {
   return (
-    <div className="flex w-320 flex-col gap-y-16">
-      <Textarea rows={1} />
-      <Textarea />
-      <Textarea className="resize-none" rows={10} />
+    <div className="flex w-320 flex-col">
+      <Label className="mb-4" htmlFor="row-1">
+        Row 1
+      </Label>
+      <Textarea className="mb-16" id="row-1" name="row-1" rows={1} />
+      <Label className="mb-4" htmlFor="row-default">
+        Row default
+      </Label>
+      <Textarea className="mb-16" id="row-default" name="row-default" />
+      <Label className="mb-4" htmlFor="row-10">
+        Row 10
+      </Label>
+      <Textarea
+        className="mb-16 resize-none"
+        id="row-10"
+        name="row-10"
+        rows={10}
+      />
+      <Label className="mb-4" htmlFor="disabled">
+        Disabled
+      </Label>
+      <Textarea className="mb-16" disabled id="disabled" name="disabled" />
     </div>
   )
 }
