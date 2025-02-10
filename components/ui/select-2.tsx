5d32aa216b0c8ecbf1703fd4b267b597493a3469
Natt Nguyen <natt.lefevre@gmail.com>
2025-02-10 11:58:00 -0800

    progress

--- components/ui/select-2.tsx
+++ components/ui/select-2.tsx
@@ -6,23 +6,23 @@ import { Select } from "radix-ui"
 export default function Select2() {
   return (
     <Select.Root defaultValue="apple">
-      <Select.Trigger className="d rounded-8 border-gray-5 bg-gray-2 flex h-40 min-w-256 items-center justify-between border border-solid px-12">
+      <Select.Trigger className="rounded-8 border-gray-5 bg-gray-2 outline-primary-9 flex h-40 min-w-256 items-center justify-between border border-solid px-12 outline-0 outline-offset-2 select-none focus-visible:outline-2">
         Apple
         <CaretDown />
       </Select.Trigger>
 
       <Select.Content
-        className="rounded-8 border-gray-5 bg-gray-2 w-full border border-solid p-2"
+        className="rounded-8 border-gray-5 bg-gray-2 w-256 overflow-hidden border border-solid"
         position="popper"
       >
         <Select.Item
-          className="text-14 hover:bg-gray-3 rounded-8 cursor-pointer px-12 py-4 transition-colors"
+          className="text-14 hover:bg-gray-3 focus-visible:bg-gray-3 bg-gray-2 flex h-40 cursor-pointer items-center px-12 outline-0 transition-colors select-none"
           value="apple"
         >
           Apple
         </Select.Item>
         <Select.Item
-          className="text-14 hover:bg-gray-3 rounded-8 cursor-pointer px-12 py-4 transition-colors"
+          className="text-14 hover:bg-gray-3 focus-visible:bg-gray-3 bg-gray-2 flex h-40 cursor-pointer items-center px-12 outline-0 transition-colors select-none"
           value="orange"
         >
           Orange
--- components/ui/theme.ts
+++ components/ui/theme.ts
@@ -15,8 +15,9 @@ export const theme = {
   duration: "duration-150",
   outline: [
     "focus-visible:outline-2",
-    "focus-visible:outline-offset-2",
-    "focus-visible:outline-primary-9",
+    "outline-0",
+    "outline-offset-2",
+    "outline-primary-9",
   ].join(" "),
   rounded: "rounded-8",
   shadow: "shadow-1",
