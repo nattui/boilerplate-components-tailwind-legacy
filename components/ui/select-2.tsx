5712ae3c03a54645ac47d12d9607677f3f623fd2
Natt Nguyen <natt.lefevre@gmail.com>
2025-02-10 12:02:09 -0800

    gap

--- components/ui/select-2.tsx
+++ components/ui/select-2.tsx
@@ -2,28 +2,32 @@
 
 import { CaretDown } from "@phosphor-icons/react"
 import { Select } from "radix-ui"
+import { useState } from "react"
 
 export default function Select2() {
+  const [value, setValue] = useState("Apple")
+
   return (
-    <Select.Root defaultValue="apple">
-      <Select.Trigger className="rounded-8 border-gray-5 bg-gray-2 outline-primary-9 flex h-40 min-w-256 items-center justify-between border border-solid px-12 outline-0 outline-offset-2 select-none focus-visible:outline-2">
-        Apple
+    <Select.Root defaultValue="apple" onValueChange={setValue}>
+      <Select.Trigger className="rounded-8 border-gray-5 bg-gray-2 outline-primary-9 shadow-1 flex h-40 min-w-256 items-center justify-between border border-solid px-12 outline-0 outline-offset-2 select-none focus-visible:outline-2">
+        {value}
         <CaretDown />
       </Select.Trigger>
 
       <Select.Content
-        className="rounded-8 border-gray-5 bg-gray-2 w-256 overflow-hidden border border-solid"
+        className="rounded-8 border-gray-5 bg-gray-2 shadow-1 w-256 overflow-hidden border border-solid"
         position="popper"
+        sideOffset={4}
       >
         <Select.Item
           className="text-14 hover:bg-gray-3 focus-visible:bg-gray-3 bg-gray-2 flex h-40 cursor-pointer items-center px-12 outline-0 transition-colors select-none"
-          value="apple"
+          value="Apple"
         >
           Apple
         </Select.Item>
         <Select.Item
           className="text-14 hover:bg-gray-3 focus-visible:bg-gray-3 bg-gray-2 flex h-40 cursor-pointer items-center px-12 outline-0 transition-colors select-none"
-          value="orange"
+          value="Orange"
         >
           Orange
         </Select.Item>
