319533fe7b9b4d792aaeeec4a8aefa3e54ce0d45
Natt Nguyen <natt.lefevre@gmail.com>
2025-02-10 11:42:25 -0800

    select progress

--- components/ui/select-2.tsx
+++ components/ui/select-2.tsx
@@ -6,19 +6,25 @@ import { Select } from "radix-ui"
 export default function Select2() {
   return (
     <Select.Root defaultValue="apple">
-      <Select.Trigger className="d rounded-8 border-gray-5 flex h-40 min-w-256 items-center justify-between border border-solid px-12">
+      <Select.Trigger className="d rounded-8 border-gray-5 bg-gray-2 flex h-40 min-w-256 items-center justify-between border border-solid px-12">
         Apple
         <CaretDown />
       </Select.Trigger>
 
       <Select.Content
-        className="rounded-8 border-gray-5 bg-gray-1 w-full border border-solid p-2"
+        className="rounded-8 border-gray-5 bg-gray-2 w-full border border-solid p-2"
         position="popper"
       >
-        <Select.Item className="px-12 py-4" value="apple">
+        <Select.Item
+          className="text-14 hover:bg-gray-3 rounded-8 cursor-pointer px-12 py-4 transition-colors"
+          value="apple"
+        >
           Apple
         </Select.Item>
-        <Select.Item className="px-12 py-4" value="orange">
+        <Select.Item
+          className="text-14 hover:bg-gray-3 rounded-8 cursor-pointer px-12 py-4 transition-colors"
+          value="orange"
+        >
           Orange
         </Select.Item>
       </Select.Content>
