9dd31b498a18728a92b733f0f037abf108ba1823
Natt Nguyen <natt.lefevre@gmail.com>
2025-02-16 10:15:28 -0800

    value

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -6,13 +6,15 @@ export interface BadgeProps {
 }
 
 export default function Badge(props: BadgeProps) {
-  const { children, value, ...rest } = props
+  const { children, value = "", ...rest } = props
 
   return (
     <div className="relative" {...rest}>
-      <div className="bg-primary-9 text-12/100 text-gray-1 absolute -top-10 -right-10 z-10 flex h-20 min-w-20 items-center justify-center rounded-full px-4">
-        {value}
-      </div>
+      {value && (
+        <div className="bg-primary-9 text-12/100 text-gray-1 absolute -top-10 -right-10 z-10 flex h-20 min-w-20 items-center justify-center rounded-full px-4">
+          {value}
+        </div>
+      )}
       {children}
     </div>
   )
