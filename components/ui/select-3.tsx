40a3609f70dc678cd6ddbe9a4db052266f5302c4
Natt Nguyen <natt.lefevre@gmail.com>
2025-02-17 20:36:08 -0800

    close

--- components/ui/select-3.tsx
+++ components/ui/select-3.tsx
@@ -12,12 +12,12 @@ export default function Select3() {
 
   return (
     <label
-      className="border-gray-5 shadow-1 hover:border-gray-8 data-[focused=true]:border-gray-12 relative flex h-40 w-full cursor-text items-center border border-solid transition-colors"
+      className="border-gray-5 shadow-1 hover:border-gray-8 data-[focused=true]:border-gray-12 relative flex h-40 w-full cursor-text items-center border border-solid pr-32 pl-12 transition-colors"
       data-focused={isFocused}
       htmlFor={id}
     >
       <input
-        className="h-full w-full px-12 outline-0"
+        className="h-full w-full outline-0"
         id={id}
         onBlur={() => setIsFocused(false)}
         onChange={(event) => setValue(event.target.value)}
@@ -25,11 +25,18 @@ export default function Select3() {
         type="text"
         value={value}
       />
-      <span>test</span>
 
-      <IconButton size="small" variant="ghost">
-        <LucideX size={16} />
-      </IconButton>
+      {/* <span>test</span> */}
+      {value && (
+        <IconButton
+          className="absolute right-0"
+          onClick={() => setValue("")}
+          size="small"
+          variant="ghost"
+        >
+          <LucideX size={16} />
+        </IconButton>
+      )}
     </label>
   )
 }
