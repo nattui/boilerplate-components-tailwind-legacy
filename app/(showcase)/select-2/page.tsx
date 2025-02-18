fc69cb7b2658c76ef2ab190cf734666110cc49b1
Natt Nguyen <natt.lefevre@gmail.com>
2025-02-17 16:54:34 -0800

    select 2

--- app/(showcase)/select-2/page.tsx
+++ app/(showcase)/select-2/page.tsx
@@ -2,8 +2,8 @@ import Select2 from "@/components/ui/select-2"
 
 export default function Inputs2Page() {
   return (
-    <>
+    <div className="w-256">
       <Select2 />
-    </>
+    </div>
   )
 }
--- components/ui/select-2.tsx
+++ components/ui/select-2.tsx
@@ -1,37 +1,49 @@
 "use client"
 
-import { LucideChevronDown } from "lucide-react"
+import { ChevronDownIcon } from "lucide-react"
 import { Select } from "radix-ui"
-import { useState } from "react"
 
 export default function Select2() {
-  const [value, setValue] = useState("Apple")
-
   return (
-    <Select.Root defaultValue="apple" onValueChange={setValue}>
-      <Select.Trigger className="rounded-8 border-gray-5 bg-gray-2 outline-primary-9 shadow-1 flex h-40 min-w-256 cursor-pointer items-center justify-between border border-solid px-12 outline-0 outline-offset-2 select-none focus-visible:outline-2">
-        {value}
-        <LucideChevronDown size={16} />
+    <Select.Root defaultValue="light">
+      <Select.Trigger className="rounded-8 border-gray-5 outline-primary-9 data-[active=true]:border-gray-8 active:border-gray-8 disabled:!border-gray-5 focus:border-gray-8 hover:border-gray-8 text-gray-12 text-14 font-400 shadow-1 flex h-40 w-full cursor-pointer items-center justify-between border border-solid px-12 leading-150 outline-0 outline-offset-2 transition-[background-color,border-color,opacity] duration-150 focus-visible:outline-2">
+        <Select.Value placeholder="Theme" />
+
+        <Select.Icon asChild>
+          <ChevronDownIcon size={16} />
+        </Select.Icon>
       </Select.Trigger>
 
-      <Select.Content
-        className="rounded-8 border-gray-5 bg-gray-2 shadow-1 w-256 overflow-hidden border border-solid"
-        position="popper"
-        sideOffset={4}
-      >
-        <Select.Item
-          className="text-14 hover:bg-gray-3 focus-visible:bg-gray-3 bg-gray-2 flex h-40 cursor-pointer items-center px-12 outline-0 transition-colors select-none"
-          value="Apple"
+      <Select.Portal>
+        <Select.Content
+          className="border-gray-5 rounded-8 shadow-1 max-h-(--radix-select-content-available-height) w-(--radix-select-trigger-width) border border-solid p-4"
+          position="popper"
+          sideOffset={4}
         >
-          Apple
-        </Select.Item>
-        <Select.Item
-          className="text-14 hover:bg-gray-3 focus-visible:bg-gray-3 bg-gray-2 flex h-40 cursor-pointer items-center px-12 outline-0 transition-colors select-none"
-          value="Orange"
-        >
-          Orange
-        </Select.Item>
-      </Select.Content>
+          <Select.Viewport>
+            <Select.Item
+              className="data-[highlighted]:bg-gray-3 hover:bg-gray-3 rounded-8 font-500 hover:text-gray-12 flex cursor-pointer items-center gap-x-8 px-8 py-6 transition-colors outline-none select-none"
+              value="light"
+            >
+              <Select.ItemText>Light</Select.ItemText>
+            </Select.Item>
+
+            <Select.Item
+              className="data-[highlighted]:bg-gray-3 hover:bg-gray-3 rounded-8 font-500 hover:text-gray-12 flex cursor-pointer items-center gap-x-8 px-8 py-6 transition-colors outline-none select-none"
+              value="dark"
+            >
+              <Select.ItemText>Dark</Select.ItemText>
+            </Select.Item>
+
+            <Select.Item
+              className="data-[highlighted]:bg-gray-3 hover:bg-gray-3 rounded-8 font-500 hover:text-gray-12 flex cursor-pointer items-center gap-x-8 px-8 py-6 transition-colors outline-none select-none"
+              value="system"
+            >
+              <Select.ItemText>System</Select.ItemText>
+            </Select.Item>
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
     </Select.Root>
   )
 }
